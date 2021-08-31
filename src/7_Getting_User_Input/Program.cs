83d0c5c10a132d6f72217f4864d77c67b13c17e3
Amazing Cabiles <clappalc12@gmail.com>
2021-08-31 18:05:09 +0800

    08/31/2021

--- src/7_Getting_User_Input/Program.cs
+++ src/7_Getting_User_Input/Program.cs
@@ -6,7 +6,29 @@ namespace _7_Getting_User_Input
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Write prints it out on the same line
+            // Gets name input
+            Console.Write("Enter your name: ");
+
+            // ReadLine also takes user input
+            // stores the string input in the variable called name 
+            string name = Console.ReadLine();
+
+            // Gets age input
+            Console.Write("Enter your age: ");
+
+            // stores the string input in the variable called age 
+            string age = Console.ReadLine();
+
+            // Prints name input
+            // Console.WriteLine("Hello " + name);
+
+            // Prints name and age input
+            Console.WriteLine("Hello " + name + " you are " + age);
+
+            // Stops the program from closing as soon as executed (just like ReadLine)
+            // Press any key on the keyboard to close the program 
+            Console.ReadKey();
         }
     }
 }
