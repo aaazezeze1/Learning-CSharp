3aeb8aff9f6850ffb03db3a9c79151b3cba03dd1
Amazing Cabiles <clappalc12@gmail.com>
2021-08-31 18:14:10 +0800

    08/31/2021

---
+++ src/9_Building_a_Mad_Lib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _9_Building_a_Mad_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
