b1126b0708f1a7835b8f7e36482e45427b22e388
Amazing Cabiles <clappalc12@gmail.com>
2021-08-31 20:43:08 +0800

    08/31/2021

---
+++ src/10_Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An array of integers
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            //     array index = {0, 1, 2,  3,  4,  5 }
+
+            // Accessing individual elements in an array
+            Console.WriteLine(luckyNumbers[0]);
+            Console.WriteLine(luckyNumbers[2]);
+
+            // Changing an element in an array
+            luckyNumbers[1] = 900;
+            Console.WriteLine(luckyNumbers[1]);
+
+            // An array of strings
+            // an array that doesn't have elements inside it, just yet
+            // this array can store 5 elements 
+            string[] friends = new string[5];
+
+            // Giving values to an array
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.ReadLine();
+        }
+    }
+}
--- src/8_Building_a_Calculator/Program.cs
+++ src/8_Building_a_Calculator/Program.cs
@@ -6,7 +6,27 @@ namespace _8_Building_a_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // To convert string number to integer number
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            // Adding integers together
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            // Adding decimals together
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
         }
     }
 }
