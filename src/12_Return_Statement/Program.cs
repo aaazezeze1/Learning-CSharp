722be7cbac467965c120e2c9e25d8516585afe4e
Amazing Cabiles <clappalc12@gmail.com>
2021-09-11 16:39:11 +0800

    09/11/2021

---
+++ src/12_Return_Statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12_Return_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To see the output returned to the computer
+            Console.WriteLine(cube(5));
+
+            // does the same thing but you can modify the value using the integer cubedNumber
+            int cubedNumber = cube(5);
+            Console.WriteLine(cubedNumber);
+
+            Console.ReadLine();
+        }
+        static int cube(int num)
+        {
+            int result = num * num * num;
+            // returns value to computer not user
+            return result;
+        }
+    }
+}
