58f66c2d50e0f62cd96a14ae23d942bd8b041c6b
Amazing Cabiles <clappalc12@gmail.com>
2021-12-05 21:13:58 +0800

    12/05/21

---
+++ src/14_Building_a_Better_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14_Building_a_Better_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
