450d7b218c3fea4f3f388ced6b72dc9546bf25bd
Amazing Cabiles <clappalc12@gmail.com>
2021-12-05 21:14:52 +0800

    12/05/21

--- src/14_Building_a_Better_Calculator/Program.cs
+++ src/14_Building_a_Better_Calculator/Program.cs
@@ -6,7 +6,37 @@ namespace _14_Building_a_Better_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
         }
     }
 }
