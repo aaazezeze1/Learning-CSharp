de83c743a6a17c5a3ef8606f84b50b4d5c84c102
Amazing Cabiles <amazinggracecabiles@gmail.com>
2023-03-11 17:10:49 +0800

    03/11/2023

---
+++ src/15_Switch_Statements/Program.cs
@@ -0,0 +1,36 @@
+﻿static string GetDay(int dayNum)
+{
+    string dayName;
+
+    switch (dayNum)
+    {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 3:
+            dayName = "Tuesday";
+            break;
+        case 4:
+            dayName = "Wednesday";
+            break;
+        case 5:
+            dayName = "Thursday";
+            break;
+        case 6:
+            dayName = "Friday";
+            break;
+        case 7:
+            dayName = "Saturday";
+            break;
+        default:
+            dayName = "Invalid Day Number";
+            break;
+    }
+
+    return dayName;
+}
+
+Console.WriteLine(GetDay(0));
